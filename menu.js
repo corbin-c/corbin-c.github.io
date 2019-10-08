2998552885990989f3ce1a83b2845bd02f4eda11
Clément Corbin <corbin.clement@gmail.com>
2019-10-08 19:49:33 +0200

    simplified menu script

--- menu.js
+++ menu.js
@@ -1,53 +1,28 @@
-async function parse_response(ressource)
-{
-	ressource =  await ressource.blob()
-	url = URL.createObjectURL(ressource);
-	return new Promise(function (resolve, reject) {
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.onreadystatechange = function() {
-		if (this.readyState == 4) {
-				resolve(xmlhttp)
-			}
-	  };
-	xmlhttp.open("GET", url, true);
-	xmlhttp.send();
-	});	
-}
-function load(url)
-{
-	return new Promise(function (resolve,reject)
-	{
-		var myHeaders = new Headers();
-		var myInit = { method: "GET",
-		   headers: myHeaders,
-		   mode: "cors",
-		   withCredentials: true,
-		   cache: "no-cache" };
-	var myRequest = new Request(url,myInit);
-	resolve(fetch(myRequest,myInit).catch(function(){reject(false)}))
-	})
-}
-async function menu()
-{
-	var user = "corbin-c";
-	var sibling = document.querySelector("#menujs");
-	var menu = document.createElement("ul");
-	user = await load("https://api.github.com/users/"+user+"/repos")
-	user = await parse_response(user);
-	user = JSON.parse(user.response);
-	sibling.parentElement.insertBefore(menu,sibling);
-	for (i in user)
-	{
-		if ((user[i].homepage != null) && (typeof user[i].homepage !== "undefined") && (user[i].homepage != ""))
-		{
-			var li = document.createElement('li');
-			var a = document.createElement('a');
-			a.href = user[i].homepage;
-			a.title = ((user[i].description != null) && (typeof user[i].description !== "undefined") && (user[i].description != "")) ? user[i].description:user[i].name;
-			a.innerHTML = user[i].name;
-			li.append(a)
-			menu.append(li)
-		}
-	}
-}
-menu();
+const USER = "corbin-c";
+(async () => {
+  let sibling = document.querySelector("#menujs");
+  let menu = document.createElement("nav");
+  let list = document.createElement("ul");
+  menu.append(list);
+  user = await fetch("https://api.github.com/users/"+USER+"/repos");
+  user = await user.json();
+  sibling.parentElement.insertBefore(menu,sibling);
+  user.map(repo => {
+    if ((repo.homepage != null)
+      && (typeof repo.homepage !== "undefined")
+      && (repo.homepage != "")
+      && (location.href.indexOf(repo.homepage) < 0)) {
+      let li = document.createElement("li");
+      let a = document.createElement("a");
+      a.href = repo.homepage;
+      a.title = ((repo.description != null)
+        && (typeof repo.description !== "undefined")
+        && (repo.description != ""))
+        ? repo.description
+        :repo.name;
+      a.innerHTML = repo.name;
+      li.append(a)
+      list.append(li)
+    }
+  });
+})();
